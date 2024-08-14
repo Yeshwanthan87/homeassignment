a62111822e99b95bc77b1af40448294eba37ab90
Yeshwanthan87 <yeshwanthan87@gmail.com>
2024-08-14 11:42:57 +0530

    change webhoookcheck

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def get_public_ip():
     ip = requests.get('https://api.ipify.org').text
-    return f'Your Public IP is: {ip} and that is a project for BIGID webhooktest'
+    return f'Your Public IP is: {ip} and that is a project for BIGID bigwebhooktest'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

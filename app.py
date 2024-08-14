d03a38471d617505e77bb4ee892b5d1a933c9bb3
Yeshwanthan87 <yeshwanthan87@gmail.com>
2024-08-14 13:32:57 +0530

    Changing message Yeshwanth

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def get_public_ip():
     ip = requests.get('https://api.ipify.org').text
-    return f'Your Public IP is: {ip} and that is a project for BIGID Assignment'
+    return f'Your Public IP is: {ip} and that is a project for BIGID Assignment Yeshwanth'
 
 
 if __name__ == '__main__':
